f97fda935b91b6629ed024a3dc4294391ed47240
mahmoud <mahmoud11magd@gmail.com>
2022-04-12 00:23:56 +0200

    first commit

---
+++ assets/js/style-mahmoud.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $( ".hover-cv" ).hover(
+//     function() {
+//       $( ".hover-cv" ).attr("data-targetid", "#exampleModa2")
+//     }, function() {
+      
+//     }
+//   );
+// });
